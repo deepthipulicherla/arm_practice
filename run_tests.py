fdb362e72b988289ee170d008183a5a669f76ad1
deepthi-Pulicherla <75684075+deepthi-Pulicherla@users.noreply.github.com>
2026-05-12 08:43:38 +0200

    skipped the thread based

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+import os
+import shutil
 
 def run(cmd):
     result = subprocess.run(cmd,shell=True)
@@ -7,19 +9,25 @@ def run(cmd):
     if result.returncode != 0:
         sys.exit(result.returncode)
 
+def generate_allure_report():
+    """Generate allure-report folder without starting a server."""
+    if os.path.exists("allure-report"):
+        shutil.rmtree("allure-report")
+
+    run("allure generate allure-results -o allure-report --clean")
 def main():
     mode = sys.argv[1] if len(sys.argv)>1 else "default"
 
     if mode == "parallel":
         run("pytest -n 4 -vv -s --alluredir=allure-results")
-        run("allure serve allure-results")
+        generate_allure_report()
 
     elif mode == "debug" :
         run("pytest -vv -s --pdb")
 
     elif mode == "regression":
         run("pytest -m regression -vv -s --alluredir=allure-results")
-        run("allure serve allure-results")
+        generate_allure_report()
 
     elif mode == "smoke":
         run("pytest -m smoke -vv -s --alluredir=allure-results")
@@ -28,6 +36,7 @@ def main():
     elif mode == "jenkins":
         print("Jenkins mode")
         run("pytest -vv -s  --alluredir=allure-results")
+        generate_allure_report()
 
     else:
         run("pytest -vv -s --alluredir=allure-results")
